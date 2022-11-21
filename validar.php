a3443d03a9cd26b5ac9fc90116f24ee6c3adca7d
marcelodanelon <marcelodanelon11@gmail.com>
2022-11-21 19:25:40 -0300

    troca de nome usuário

--- validar.php
+++ validar.php
@@ -2,7 +2,7 @@
 $usuario = $_POST['usuario'];
 $senha = $_POST['senha'];
 
-if($senha == 123 && $usuario == 'guto'){
+if($senha == 123 && $usuario == 'marcelo'){
     header('Location: painel.php');
 }else{
     header('Location: usuario-invalido.php');
